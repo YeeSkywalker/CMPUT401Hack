efd0bd83d545f7b1ca68161b1b0e8a7191fef71b
Chenxuan Wang <cxleoo9@gmail.com>
2023-01-15 10:45:36 -0700

    Add the origin of the trip

--- tripPlanning/main/models.py
+++ tripPlanning/main/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Trip(models.Model):
     addedTime = models.DateTimeField(default=datetime.now())
+    origin = models.CharField(max_length=100,default='null')
     destination = models.CharField(max_length=100)
     departureDate = models.DateTimeField(default='')
 
